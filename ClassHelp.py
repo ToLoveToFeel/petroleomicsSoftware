ed8b15c35990173c7afbc555da93177158f1b63f
i-love-linux <1137247975@qq.com>
2020-03-31 11:19:53 +0800

    ClassHelp完成

--- ClassHelp.py
+++ ClassHelp.py
@@ -98,26 +98,97 @@ class ClassHelp:
                       "碳的数目，仪器离子模式），生成的数据最终存储在excel中。这一步的目的：为后续分子式匹配做准备。"
             self.EditShow("数据库生成", explain)
         elif self.__function == "EditHelpDeleteIso":
-            explain = "这是一个参数设置界面，我们根据（去空白后的）样本文件里的内容根据Mass去匹配数据库，如果" \
-                      "未匹配到，当前Mass结束，继续（去空白后的）样本文件下一条记录的匹配；如果匹配到，则计算匹配到的分" \
-                      "子式分子量为12的C元素替换为分子量为13的C元素，分别替换一个和两个，根据替换后分子量在（去空白后的）" \
-                      "样本文件中搜索是否有对应的记录，并记录下来。这个步骤最终生成一个excel文件。"
+            explain = "这是一个参数设置界面，这一步需要“（去空白后的）样本文件”（记为file_A）和生成的数据库文件，我们根据file_A里的内" \
+                      "容根据Mass去匹配数据库，如果未匹配到，当前Mass结束，不记录任何信息，继续file_A中下一条记录的匹配；如果匹配到，则计算匹" \
+                      "配到的分子式分子量为12的C元素替换为分子量为13的C元素，分别替换一个和两个，根据替换后分子量在file_A中搜" \
+                      "索是否有对应的记录，并记录下来；信息格式为（SampleMass，SampleIntensity，Class, Neutral, Formula," \
+                      " Calc m/z, C, ion），即（file_A中的Mass，file_A中的Intensity，类别，不饱和度，分子式，质荷比，碳的数目，仪器" \
+                      "离子模式）；如果搜到同位素，则记下来，格式为（Mass，Intensity，iostope）。这个步骤最终生成一个excel文件，excel中" \
+                      "的内容都是匹配到分子的记录。"
             self.EditShow("搜同位素", explain)
         elif self.__function == "EditHelpPeakDis":
-            explain = "这是一个参数设置界面，"
+            explain = "这是一个参数设置界面，这一步需要“搜同位素后的结果”（记为file_B）和总离子流图文件（TIC），我们根据file_B中的" \
+                      "SampleMass去TIC中搜索是否有基本连续的Mass，并记录TIC中连续的Mass，信息格式为：（SampleMass，Area，startRT，" \
+                      "startRTValue，endRT，endRTValue，TICMassMedian，Class，Neutral DBE，Formula，Calc m/z，C，ion），即（file_B" \
+                      "中的Mass，TIC中区间[startRT,endRT]对应的Intensity之和，TIC中连续Mass开始的索引（从0开始，到扫描点个数-1结束），" \
+                      "开始索引对应的保留时间（RT）的值，TIC中连续Mass结束的索引，结束索引对应的保留时间（RT）的值，TIC中区间[startRT," \
+                      "endRT]对应的Mass大于该区间中最大Mass的60%的数据集合的中位数，类别，不饱和度，分子式，质荷比，碳的数目，仪器离子" \
+                      "模式）。这个步骤最终生成一个excel文件。"
             self.EditShow("峰匹配", explain)
         elif self.__function == "EditHelpRFP":
-            explain = "这是一个参数设置界面，"
+            explain = "这是一个参数设置界面，这一步需要“搜同位素后的结果”（记为file_B）或者“峰匹配后的结果”（记为file_C），根据不饱" \
+                      "和度（Neutral DBE）以及碳数（C）的连续性，判断是否需要去除，不满足连续条件的会被删除。这个步骤最终生成一个excel文" \
+                      "件，格式和file_B或者file_C的格式一致。"
             self.EditShow("去假阳性", explain)
         elif self.__function == "EditHelpPeakDiv":
-            explain = "这是一个参数设置界面，"
+            explain = "这是一个参数设置界面，这一步需要“去假阳性后的结果（并且去假阳性需要选择的是“峰匹配后的结果”）”（记为file_D），" \
+                      "我们已经进行了峰匹配，根据峰匹配的功能解释，我们相当于总离子流图（TIC）提取出一个个提取离子流图（EIC），这一步的" \
+                      "目的就是：提取出每一个m/z的色峰谱，并计算峰面积。"
             self.EditShow("峰识别", explain)
         elif self.__function == "PlotHelp":
-            pass
+            dialog = self.CreateDialog("绘图帮助", 60, 40)
+            dialog.setWindowOpacity(0.94)  # 设置透明度
+
+            label = self.GetQLabel("菜单栏 “绘图->添加” 下解释：", "color:red; font-size:15pt")
+            textEdit = QTextEdit()
+            textEdit.setFocusPolicy(Qt.NoFocus)  # 禁止编辑
+            textEdit.setFont(QFont(ConstValues.PsSetupFontType, ConstValues.PsSetupFontSize))
+            # 显示内容编辑
+            showStr = "需要去假阳性后生成的文件，去假阳性选择了哪个文件，就选择该文件去假阳性之后" \
+                      "的文件进行可视化。"
+            plot1Explain = "Class distribution：比较不同种类杂原子化合物（class）的相对含量差异。"
+            plot2Explain = "DBE distribution by class：比较具有相同class的化合物，DBE的分布情况。"
+            plot3Explain = "Carbon number distribution by class and DBEn：比较相同相同class且相" \
+                           "同DBE的化合物碳数分布情况（根据这个情况，可以推测一定化合物结构信息）。"
+            plot4Explain = "DBE vs carbon number by class：比较相同class化合物的DBE和碳数分布情况。"
+            plot5Explain = "Kendrick mass defect （KMD）：KMD图（辅助判断排除假阳性的充分性）。"
+            plot6Explain = "Retention time vs carbon number：同系物之间保留时间和碳数的关系（" \
+                           "体现液相色谱方法的优越性）。"
+            taskbarExplain = "工具栏和菜单栏对应功能一致。"
+            showStr = showStr + "\n\n" + plot1Explain + "\n\n" + plot2Explain + "\n\n" + plot3Explain + \
+                      "\n\n" + plot4Explain + "\n\n" + plot5Explain + "\n\n" + plot6Explain + "\n\n" + taskbarExplain
+            textEdit.setPlainText(showStr)
+
+            layout = QGridLayout(dialog)  # 创建栅格布局
+            # 栅格布局添加控件
+            layout.addWidget(label, 0, 0, 1, 1)
+            layout.addWidget(textEdit, 1, 0, 1, 1)
+            dialog.exec()  # 运行
         elif self.__function == "ModeHelp":
-            pass
+            dialog = self.CreateDialog("模式选择帮助", 60, 40)
+            dialog.setWindowOpacity(0.94)  # 设置透明度
+
+            label = self.GetQLabel("工具栏 “模式选择” 解释：", "color:red; font-size:15pt")
+            textEdit = QTextEdit()
+            textEdit.setFocusPolicy(Qt.NoFocus)  # 禁止编辑
+            textEdit.setFont(QFont(ConstValues.PsSetupFontType, ConstValues.PsSetupFontSize))
+            # 显示内容编辑
+            showStr = "模式选择：此软件一共有6个主要的步骤（除了绘图之外），有些功能可以不必运行，但某些" \
+                      "功能运行前必须运行某些功能，用户可以根据需要选择需要运行哪些内容。"
+            textEdit.setPlainText(showStr)
+
+            layout = QGridLayout(dialog)  # 创建栅格布局
+            # 栅格布局添加控件
+            layout.addWidget(label, 0, 0, 1, 1)
+            layout.addWidget(textEdit, 1, 0, 1, 1)
+            dialog.exec()  # 运行
         elif self.__function == "OtherHelp":
-            pass
+            dialog = self.CreateDialog("其他帮助", 60, 40)
+            dialog.setWindowOpacity(0.94)  # 设置透明度
+
+            label = self.GetQLabel("其他帮助：", "color:red; font-size:15pt")
+            textEdit = QTextEdit()
+            textEdit.setFocusPolicy(Qt.NoFocus)  # 禁止编辑
+            textEdit.setFont(QFont(ConstValues.PsSetupFontType, ConstValues.PsSetupFontSize))
+            # 显示内容编辑
+            showStr = "用户点击重置软件后，所有参数会重置为默认参数，并且需要重新读入各个文件才能运行"
+            textEdit.setPlainText(showStr)
+
+            layout = QGridLayout(dialog)  # 创建栅格布局
+            # 栅格布局添加控件
+            layout.addWidget(label, 0, 0, 1, 1)
+            layout.addWidget(textEdit, 1, 0, 1, 1)
+            dialog.exec()  # 运行
         elif self.__function == "About":
             dialog = self.CreateDialog("关于", 20, 10)
             label = self.GetQLabel("石油组学软件：v1.0", alignment="AlignCenter")  # 创建label
